48e2f29f9f787d8024d1f1c612c043ae6ce73028
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-14 13:20:03 +0530

    game completed

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turnO = true; //playerX, playerO
 
@@ -14,10 +17,15 @@ const winPatterns = [
     [6,7,8],
 ];
 
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+    closeModal();
+};
 
 boxes.forEach((box) => {
     box.addEventListener("click", () => {
-        console.log("bofdx");
         if(turnO) {  //playerO
             box.innerText ="O";
             turnO=false;
@@ -25,5 +33,58 @@ boxes.forEach((box) => {
             box.innerText ="X";
             turnO=true;
         }
+
+        box.disabled = true;
+
+        checkWinner();
+
+
     });
 });
+
+const disableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = true;
+    }
+}
+
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
+    }
+}
+
+const showWinner = (winner) => {
+    msg.innerText = `Congratulations , winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+    openModal();
+}
+
+const checkWinner = () => {
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+
+        if(pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                showWinner(pos1Val);
+            }
+        }
+    }
+};
+
+newGameBtn.addEventListener("click", resetGame);
+resetBtn.addEventListener("click", resetGame);
+
+function openModal() {
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display='flex';
+}
+
+function closeModal() {
+    const overlayElement = document.getElementById('overlay');
+    overlayElement.style.display='none';
+}
